26e32aa47c14badbacdcdfb7dced40e20b309cf5
Victor Manuel Pelaez <victor.m.pelaez@outlook.com>
2015-01-24 20:18:45 +0100

    Files

--- Practical Machine Learning/Assignment.R
+++ Practical Machine Learning/Assignment.R
@@ -1,20 +1,18 @@
 
-pml_assign <- function() {
+ pml_assign <- function() {
     
     library(caret); library(rattle);
     library(rpart);library(rpart.plot);
     library(randomForest);
-    ##training<-read.csv("./trainingdata.csv")
-    ##testing_FINAL<-read.csv("./testingdata.csv")
     
-    setwd("H:/Mis Documentos/github/Practical Machine Learning/Prediction Assignment Writeup")
-    data <- read.table("pml-training.csv",header=T,sep=",")
-    data_test <- read.table("pml-testing.csv",header=T,sep=",")   
+    data <- read.table("./pml-training.csv",header=T,sep=",")
+    data_test <- read.table("./pml-testing.csv",header=T,sep=",")   
  
     inTrain  <- createDataPartition(y=data$classe,p=0.6,list=FALSE);
     training <- data[inTrain,]
     testing  <- data[-inTrain,]
     
+    ## Check correlation in data and check features
     cls_list <- lapply(data,class)
     cls_numeric <-which(cls_list=='numeric')
     M <-abs(cor(data[,cls_numeric]))
@@ -22,26 +20,24 @@ pml_assign <- function() {
     which(M>0.8,arr.ind=T)
     
     ## Preprocesing data set
-    #cls_listTrain <- lapply(training,class)
-    #cls_numericTrain <-which(cls_listTrain=='numeric')
-    #PTraining<- training[,cls_numericTrain]  #processed data training set
-    
     PTraining<- training[,-c(1:7)]  #processed data training set
     indZV<-nearZeroVar(PTraining, saveMetrics=TRUE)
     PTraining<-PTraining[,!indZV$nzv]
     
-    columnNumbersToRemove<-vector()
-    vectorIndex<-0
+    columnNumbersToRemove<-vector() #Empty vector
+    Indx<-0
     for(i in 1:ncol(PTraining)){
         if((sum(is.na(PTraining[,i]))/nrow(PTraining))>=0.60){
-            vectorIndex<-vectorIndex+1
-            columnNumbersToRemove[vectorIndex]<-i
+            Indx<-Indx+1
+            columnNumbersToRemove[Indx]<-i
         }
     }
     PTraining<-PTraining[,-columnNumbersToRemove]
 
     
+    ## Set a seed and train a model
     set.seed(123)
+    # Decision tree
     modelFit <- train(classe ~. , data=PTraining, method='rpart')
     fancyRpartPlot(modelFit$finalModel)
     
@@ -49,18 +45,16 @@ pml_assign <- function() {
     predictionsDT <-predict(modelFit,newdata=PTesting)
     confusionMatrix(predictionsDT, PTesting$classe)
     
-    
+    # RandomForest
     modelFit_RF <- randomForest(classe ~. , data=PTraining)
     predictionsRF <- predict(modelFit_RF, PTesting, type = "class")
 
     
-    
-    #testing_FINAL
+    ## Testing with testing_FINAL data
     variables2<-colnames(PTraining[,-53])
     finalTesting<-data_test[variables2]
     
     finalPredictionsDT<-predict(modelFit,finalTesting)
-    #return(pred.finalPredictionsDT)
     
     finalPredictionsRF<-predict(modelFit_RF,finalTesting)
     return(finalPredictionsRF)
--- Practical Machine Learning/README.md
+++ Practical Machine Learning/README.md
@@ -3,7 +3,8 @@ Prediction Assignment Writeup
 
 Assignment goal
 
-The goal of your project is to predict the manner in which they did the exercise. 
-This is the "classe" variable in the training set. You may use any of the other variables to predict with. 
-You should create a report describing how you built your model, how you used cross validation, what you think the expected out of sample error is, and why you made the choices you did.
- You will also use your prediction model to predict 20 different test cases. 
+The goal of this project is to predict the "classe" variable in the set. It may use any of the other variables to predict with. 
+Also, create a report describing how you built your model, how you used cross validation, what you think the expected out of sample error is, and why you made the choices you did.
+It will also use the prediction model to predict 20 different test cases. 
+
+
